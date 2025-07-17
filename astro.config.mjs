6d9f7d50c9cca8b8cdaf81cc08a56bef808fb9c1
Can Taşlıçukur <can.taslicukur@ozu.edu.tr>
2025-07-17 12:46:31 +0200

    fix: :bug: get rid of partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://can-taslicukur.github.io",
-  integrations: [partytown()],
 });
