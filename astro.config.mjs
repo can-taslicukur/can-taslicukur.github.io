f74e721e9d6035c174d9fc407ebb55136ac00346
Can Taşlıçukur <can.taslicukur@ozu.edu.tr>
2025-07-17 13:08:39 +0200

    build: :heavy_plus_sign: bring back partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://can-taslicukur.github.io",
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
