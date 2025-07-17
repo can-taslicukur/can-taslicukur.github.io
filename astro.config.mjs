df06e09b9beb938cad43c4d52d378d31122ec05f
Can Taşlıçukur <can.taslicukur@ozu.edu.tr>
2025-07-17 12:21:00 +0200

    ✨ feat(integrations): add Partytown for improved script loading
    Integrates Partytown to optimize third-party script execution and reduce main thread blocking. Adds Google Tag Manager setup using Partytown for better performance. Updates dependencies to include Partytown and related packages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://can-taslicukur.github.io",
+  integrations: [partytown()],
 });
